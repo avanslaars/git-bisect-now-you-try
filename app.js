8f4bab4db07e1be9a02e69856fa3294776e099f1
Andrew L. Van Slaars <avanslaars@gmail.com>
2015-12-01 21:39:49 -0500

    made some more changes

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 (function() {
   'use strict';
+  console.log('Testing')
 
   function doSomeStuff(){
     //This will eventually do some things
     return function innerFunc(fnArg){
       //TODO: Make this code do some work
       if(fnArg){
-        
+
       }
     }
   }
